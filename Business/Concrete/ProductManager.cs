2e7430cdf30389e2734494c7f3051f15298a814e
Oğuzhan Işcan <oguzhaniscn28@gmail.com>
2023-11-08 03:30:32 +0300

    msCacheAddedandTested

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,6 +2,8 @@
 using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Business;
@@ -34,6 +36,8 @@ namespace Business.Concrete
         //
         [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
+
         public IResult Add(Product product)
         {
             IResult result = BusinessRules.Run(CheckIfProductCountOfCategoryCorrect(product.CategoryId)
@@ -53,7 +57,7 @@ namespace Business.Concrete
         }
 
         //
-        //[CacheAspect] //key,value -> 
+        [CacheAspect] //key,value -> 
         public IDataResult<List<Product>> GetAll()
         {
             //if(DateTime.Now.Hour==22)
@@ -71,6 +75,7 @@ namespace Business.Concrete
         }
 
         //
+        [CacheAspect]
         public IDataResult<Product> GetById(int productId)
         {
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
@@ -91,6 +96,7 @@ namespace Business.Concrete
         //
 
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Update(Product product)
         {
 
@@ -132,5 +138,12 @@ namespace Business.Concrete
             return new SuccessResult();
         }
 
+        //
+
+        //[TransactionScopeAspect]
+        public IResult AddTransactionalTest(Product product)
+        {
+            return null;
+        }
     }
 }

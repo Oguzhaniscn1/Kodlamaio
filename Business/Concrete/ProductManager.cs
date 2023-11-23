a9c755a1df5506bcf9ad58666d0403b32d1be15e
Oğuzhan Işcan <oguzhaniscn28@gmail.com>
2023-11-23 04:06:49 +0300

    trycacheMidW

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -34,10 +34,9 @@ namespace Business.Concrete
         }
 
         //
-        [SecuredOperation("product.add,admin")]
+        ////[SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
-        [CacheRemoveAspect("IProductService.Get")]
-
+        //[CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
         {
             IResult result = BusinessRules.Run(CheckIfProductCountOfCategoryCorrect(product.CategoryId)
--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -23,6 +23,9 @@ namespace WebAPI.Controllers
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
+
+            Thread.Sleep(2000);
+
             
             var result = _productService.GetAll();
             if(result.Success)
@@ -59,6 +62,16 @@ namespace WebAPI.Controllers
 
 
 
+        [HttpGet("getbycategory")]
+        public IActionResult GetByCategory(int categoryId)
+        {
+            var result = _productService.GetAllByCategoryId(categoryId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
 
 
 
